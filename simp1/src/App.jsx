5c7881308ed6727a9998a66304a6b8f6727ff805
CLyon <66389401+ogninga@users.noreply.github.com>
2024-01-02 23:20:19 -0500

    added some code for usestate hooks

--- simp1/src/App.jsx
+++ simp1/src/App.jsx
@@ -1,12 +1,20 @@
-import { TodoList } from "./todolist";
-import { TodoListClass } from "./TodoClass";
+// import { TodoList } from "./todolist";
+// import { TodoListClass } from "./TodoClass";
 
-import { Eman } from "./Eman";
+// import { Eman } from "./Eman";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("eman");
+  function handleClick() {
+    setName("Bob");
+  }
+
   return (
     <>
-      <h1>Todo List</h1>
+      <h1 onClick={handleClick}>hi{name}</h1>
+
+      {/* <h1>Todo List</h1>
       <TodoList />
       this belongs to
       <Eman name="turtle" number={12} style="top" />
@@ -14,7 +22,7 @@ function App() {
       <Eman name="Billy bob joel" number={8} style="tux">
         Asleeeeeeeeep
       </Eman>
-      <TodoListClass />
+      <TodoListClass /> */}
     </>
   );
 }
