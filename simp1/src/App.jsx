927b83836610051066571d7b0eadca118fbb31b7
CLyon <66389401+ogninga@users.noreply.github.com>
2024-01-04 20:46:18 -0500

    started project

--- simp1/src/App.jsx
+++ simp1/src/App.jsx
@@ -1,50 +1,44 @@
-// import { TodoList } from "./todolist";
-// import { TodoListClass } from "./TodoClass";
-
-// import { Eman } from "./Eman";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState(() => {
-    console.log("name changed");
-    return "Joe, solonana";
-  });
+  const [array, setArray] = useState(["A", "B", "C", "D", "E"]);
 
-  const [age, setAge] = useState(0);
-  function handleClick() {
-    setName("Bob");
-
-    setAge((currentAge) => {
-      return currentAge + 1;
+  function removeFirstElement() {
+    setArray((currentArray) => {
+      return currentArray.slice(1);
     });
-    setAge((currentAge) => {
-      return currentAge + 1;
-    });
+  }
 
-    setCount((c) => {
-      console.log("count increased by 1");
-      return c + 1;
+  function removeLetter(letter) {
+    setArray((currentArray) => {
+      return currentArray.filter((element) => element !== letter);
+    });
+  }
+
+  function addLetterStart(letter) {
+    setArray((currentArray) => {
+      return [letter, ...currentArray];
+    });
+  }
+
+  function addLetterEnd(letter) {
+    setArray((currentArray) => {
+      return [...currentArray, letter];
     });
   }
 
   return (
     <>
-      <h1 onClick={handleClick}>
-        hi{name}
-        {age}
-        the current count is {count}
-      </h1>
-
-      {/* <h1>Todo List</h1>
-      <TodoList />
-      this belongs to
-      <Eman name="turtle" number={12} style="top" />
-      <Eman name="snake" number={23} style="tully" />
-      <Eman name="Billy bob joel" number={8} style="tux">
-        Asleeeeeeeeep
-      </Eman>
-      <TodoListClass /> */}
+      <button onClick={removeFirstElement}>Remove First Element</button>
+      <br />
+      <button onClick={() => removeLetter("A")}>Remove all Letter A</button>
+      <br />
+      <button onClick={() => addLetterStart(["R"])}>add R to start</button>
+      <br />
+      <button onClick={() => addLetterEnd("O")}>Add O to end.</button>
+      <br />
+      <br />
+      {array.join(", ")}
     </>
   );
 }
