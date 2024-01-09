f0ddfd20d63965e73996394d992dc560069517df
CLyon <66389401+ogninga@users.noreply.github.com>
2024-01-09 03:28:37 -0500

    mad button component

--- modern_bank_app/bank_app/src/components/Business.jsx
+++ modern_bank_app/bank_app/src/components/Business.jsx
@@ -1,6 +1,21 @@
 import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const Business = () => (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You can focus on running your business,{" "}
+        <br className="sm:block hidden" /> while we focus on payment processing.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        There are hundreds of credit cards on the market, with the right one you
+        can improve your credit score, earn rewards, and save money.
+      </p>
 
-)
+      <Button styles="mt-10" />
+    </div>
+  </section>
+);
 export default Business;
