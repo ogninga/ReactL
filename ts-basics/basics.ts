a82f1be78ce655b99bddb646e7176cfcc915d232
CLyon <66389401+ogninga@users.noreply.github.com>
2024-01-09 22:27:01 -0500

    added files for ts basics

---
+++ ts-basics/basics.ts
@@ -0,0 +1,18 @@
+let username: string = "jack";
+let hasLoggedIn: boolean = true;
+
+// hasLoggedIn += "Herrington";
+console.log(hasLoggedIn);
+
+let myNumber: number = 10;
+
+let myRegex: RegExp = /foo/;
+
+const names: string[] = username.split(" ");
+
+const myValues: Array<number> = [2, 1, 2, 3];
+
+const myPerson: { first: string; last: string } = {
+  first: "jack",
+  last: "herrignton",
+};
